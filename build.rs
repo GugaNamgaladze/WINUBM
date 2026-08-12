4a6a4d396c2228a178f2fc4509eade3bc4915111
pc <pc@127.0.0.1localhost>
2026-08-12 19:02:40 +0400

    commiting

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
 use std::process::Command;
 
-println!("checking if required assets are updated ")
-
 fn main() {
-Command::new("sh")
-    .args("-c")
-    .args("python3 /home/pc/WINUBM/update_assets.py")
 
-}
+println!("checking if required assets are updated");
+
+Command::new("sh")
+    .arg("-c")
+    .arg("python3 /home/pc/WINUBM/update_assets.py");
+
+} 
+
